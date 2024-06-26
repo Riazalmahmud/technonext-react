84285c583dde856de385492ec9414743dedacd35
riazalmahmud041@gmail.com <riazalmahmud041@gmail.com>
2024-06-26 22:03:43 +0600

    context

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,11 +1,39 @@
+'use client';
 import logo from "../../assets/logo.png";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { TbWorld } from "react-icons/tb";
 import "./Header.css"
+import { useEffect, useRef, useState } from "react";
 const Header = () => {
+const userRer = useRef(0)
+const [zoom, setZoom]= useState([])
+
+useEffect(()=>{
+  userRer.current++
+  setZoom(userRer.current)
+console.log(zoom)
+},[])
+
+console.log(userRer)
+
   return (
     <div>
-      <nav className="bg-white dark:bg-gray-900 start-0 border-b border-gray-200 dark:border-gray-600">
+
+      <header>
+        <nav className="flex justify-between  py-4 bg-slate-50 px-4">
+          <div >
+            <div className="logo">
+            <img className="lg:mr-8" src={logo} alt="" />
+            </div>
+          </div>
+          <div>
+
+            
+          </div>
+        </nav>
+      </header>
+
+      {/* <nav className="bg-white dark:bg-gray-900 start-0 border-b border-gray-200 dark:border-gray-600 px-4">
         <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="https://flowbite.com/"
@@ -13,7 +41,7 @@ const Header = () => {
           >
             <img className="lg:mr-8" src={logo} alt="" />
             <div
-              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 ml-8"
+              className="items-center justify-between hidden lg:block w-full md:flex md:w-auto md:order-1 ml-8"
               id="navbar-sticky"
             >
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -117,7 +145,7 @@ const Header = () => {
             </button>
           </div>
         </div>
-      </nav>
+      </nav> */}
     </div>
   );
 };
